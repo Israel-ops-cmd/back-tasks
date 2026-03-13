18851a82652472b222920984fb8991515aeaa1ec
Israel-ops-cmd <israellipe2020@gmail.com>
2026-03-13 09:24:01 -0300

    feature/api-routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { routes } from './routes.js'
 
 const app = Fastify({ logger: true })
 
 const start = async () => {
+
+    app.register(cors, {
+        origin: true,
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELTETE', 'OPTIONS']
+    })
+
+    await app.register(routes)
+    
     try {
         app.listen({ port: 3333})
     } catch(e) {
